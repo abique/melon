11628623219c3177628106a8b93411003cdee2ef
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-15 13:08:28 +0900

    dlist: added tests and dlist_insert

--- src/dlist.h
+++ src/dlist.h
@@ -19,6 +19,31 @@
     }                                                   \
   } while (0)
 
+# define melon_dlist_insert(List, Prev, Item, Prefix)   \
+  do {                                                  \
+    if (!(List))                                        \
+    {                                                   \
+      (List) = (Item);                                  \
+      (Item)->Prefix##next = (Item);                    \
+      (Item)->Prefix##prev = (Item);                    \
+    }                                                   \
+    else if (!Prev) /* push_front */                    \
+    {                                                   \
+      (Item)->Prefix##next = (List);                    \
+      (Item)->Prefix##prev = (List)->Prefix##prev;      \
+      (Item)->Prefix##next->Prefix##prev = (Item);      \
+      (Item)->Prefix##prev->Prefix##next = (Item);      \
+      (List) = (Item);                                  \
+    }                                                   \
+    else                                                \
+    {                                                   \
+      (Item)->Prefix##next = (Prev)->Prefix##next;      \
+      (Item)->Prefix##prev = (Prev);                    \
+      (Item)->Prefix##next->Prefix##prev = (Item);      \
+      (Item)->Prefix##prev->Prefix##next = (Item);      \
+    }                                                   \
+  } while (0)
+
 # define melon_dlist_unlink(List, Item, Prefix)                         \
   do {                                                                  \
     /* just 1 element */                                                \
--- tests/dlist.c
+++ tests/dlist.c
@@ -5,8 +5,8 @@
 
 typedef struct item
 {
-  struct item * next;
-  struct item * prev;
+  struct item * toto_next;
+  struct item * toto_prev;
   int value;
 } item;
 
@@ -14,8 +14,8 @@ item * item_new(int value)
 {
   item * item = malloc(sizeof (*item));
   assert(item);
-  item->next = NULL;
-  item->prev = NULL;
+  item->toto_next = NULL;
+  item->toto_prev = NULL;
   item->value = value;
   return item;
 }
@@ -32,56 +32,100 @@ int main()
     items[i] = item_new(i);
 
   /* check the empty case */
-  melon_dlist_push(list, items[0], );
+  melon_dlist_push(list, items[0], toto_);
   assert(list == items[0]);
-  assert(items[0]->next == items[0]);
-  assert(items[0]->prev == items[0]);
+  assert(items[0]->toto_next == items[0]);
+  assert(items[0]->toto_prev == items[0]);
 
-  melon_dlist_pop(list, tmp, );
+  melon_dlist_pop(list, tmp, toto_);
   assert(!list);
   assert(tmp == items[0]);
-  assert(!tmp->next);
-  assert(!tmp->prev);
+  assert(!tmp->toto_next);
+  assert(!tmp->toto_prev);
 
   /* check 2 elements case */
-  melon_dlist_push(list, items[0], );
-  melon_dlist_push(list, items[1], );
+  melon_dlist_push(list, items[0], toto_);
+  melon_dlist_push(list, items[1], toto_);
   assert(list == items[0]);
-  assert(items[0]->next == items[1]);
-  assert(items[1]->next == items[0]);
-  assert(items[0]->prev == items[1]);
-  assert(items[1]->prev == items[0]);
+  assert(items[0]->toto_next == items[1]);
+  assert(items[1]->toto_next == items[0]);
+  assert(items[0]->toto_prev == items[1]);
+  assert(items[1]->toto_prev == items[0]);
 
-  melon_dlist_pop(list, tmp, );
-  assert(!tmp->next);
-  assert(!tmp->prev);
+  melon_dlist_pop(list, tmp, toto_);
+  assert(!tmp->toto_next);
+  assert(!tmp->toto_prev);
   assert(tmp == items[0]);
   assert(list == items[1]);
-  assert(items[1]->next == items[1]);
-  assert(items[1]->prev == items[1]);
-  melon_dlist_pop(list, tmp, );
+  assert(items[1]->toto_next == items[1]);
+  assert(items[1]->toto_prev == items[1]);
+  melon_dlist_pop(list, tmp, toto_);
   assert(tmp == items[1]);
-  assert(!tmp->next);
-  assert(!tmp->prev);
+  assert(!tmp->toto_next);
+  assert(!tmp->toto_prev);
   assert(!list);
+
+  /* check insert */
+  melon_dlist_insert(list, (item*)NULL, items[1], toto_);
+  assert(list == items[1]);
+  assert(items[1]->toto_prev == items[1]);
+  assert(items[1]->toto_next == items[1]);
+
+  melon_dlist_insert(list, items[1], items[2], toto_);
+  assert(list == items[1]);
+  assert(items[1]->toto_prev == items[2]);
+  assert(items[1]->toto_next == items[2]);
+  assert(items[2]->toto_prev == items[1]);
+  assert(items[2]->toto_next == items[1]);
+
+  melon_dlist_insert(list, (item*)NULL, items[0], toto_);
+  assert(list == items[0]);
+  assert(items[0]->toto_prev == items[2]);
+  assert(items[0]->toto_next == items[1]);
+  assert(items[1]->toto_prev == items[0]);
+  assert(items[1]->toto_next == items[2]);
+  assert(items[2]->toto_prev == items[1]);
+  assert(items[2]->toto_next == items[0]);
+
+  /* check unlink */
+  melon_dlist_unlink(list, items[0], toto_);
+  assert(list == items[1]);
+  assert(!items[0]->toto_next);
+  assert(!items[0]->toto_prev);
+  assert(items[1]->toto_prev == items[2]);
+  assert(items[1]->toto_next == items[2]);
+  assert(items[2]->toto_prev == items[1]);
+  assert(items[2]->toto_next == items[1]);
+
+  melon_dlist_unlink(list, items[2], toto_);
+  assert(list == items[1]);
+  assert(items[1]->toto_prev == items[1]);
+  assert(items[1]->toto_next == items[1]);
+  assert(!items[2]->toto_next);
+  assert(!items[2]->toto_prev);
+
+  melon_dlist_unlink(list, items[1], toto_);
+  assert(!list);
+  assert(!items[1]->toto_next);
+  assert(!items[1]->toto_prev);
 
   /* push all then use pop to free */
   for (int i = 0; i < NB; ++i)
-    melon_dlist_push(list, items[i], );
+    melon_dlist_push(list, items[i], toto_);
 
   /* check */
   assert(list == items[0]);
   for (int i = 0; i < NB; ++i)
   {
-    assert(items[i]->next == items[(i + 1) % NB]);
-    assert(items[i]->prev == items[(NB + i - 1) % NB]);
+    assert(items[i]->toto_next == items[(i + 1) % NB]);
+    assert(items[i]->toto_prev == items[(NB + i - 1) % NB]);
   }
 
   /* pop and free */
   int nb = 0;
   while (1)
   {
-    melon_dlist_pop(list, tmp, );
+    melon_dlist_pop(list, tmp, toto_);
     if (!tmp)
       break;
     free(tmp);
